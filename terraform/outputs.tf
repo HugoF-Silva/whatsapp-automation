8602d13ca6c6333fc5c0d2ef239b0361223e4d3c
HugoF-Silva <hugo.fernandes@discente.ufg.br>
2025-06-30 17:22:27 -0300

    Fix output name for Redis endpoint
    Renamed the duplicate 'postgres_endpoint' output to 'redis_endpoint' to correctly reflect the Redis cache node address.

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -21,6 +21,6 @@ output "postgres_endpoint" {
   value = "postgresql://postgres:postgres123@${data.aws_db_instance.postgres.endpoint}:5432/postgres"
 }
 
-output "postgres_endpoint" {
+output "redis_endpoint" {
   value = "${data.aws_elasticache_cluster.redis.cache_nodes[0].address}:6379"
 }
