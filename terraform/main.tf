74bb0b9db0d372477317a80ed313114eebef5fd3
HugoF-Silva <hugo.fernandes@discente.ufg.br>
2025-07-01 07:30:46 -0300

    Move Lambda packaging to GitHub Actions workflow
    Removes Terraform archive_file data sources and updates Lambda function resources to use pre-built zip files from the build directory. Adds steps in the GitHub Actions workflow to package the message-checker and trigger-api Lambda functions before deployment.

--- terraform/main.tf
+++ terraform/main.tf
@@ -28,16 +28,9 @@ resource "aws_iam_role_policy_attachment" "lambda_dynamo_redis" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess"
 }
 
-### Package & deploy message-checker ###
-data "archive_file" "message_checker_zip" {
-  type        = "zip"
-  source_dir  = "${path.module}/../lambda/message-checker"
-  output_path = "${path.module}/build/message-checker.zip"
-}
-
 resource "aws_lambda_function" "message_checker" {
   function_name = "message-checker-${var.deployment_id}"
-  filename      = data.archive_file.message_checker_zip.output_path
+  filename      = "${path.module}/build/message-checker.zip"
   handler       = "handler.lambda_handler"
   runtime       = "python3.10"
   role          = aws_iam_role.lambda_exec.arn
@@ -59,16 +52,9 @@ resource "aws_lambda_function_url" "message_checker_url" {
   authorization_type = "NONE"
 }
 
-### Package & deploy trigger-api ###
-data "archive_file" "trigger_api_zip" {
-  type        = "zip"
-  source_dir  = "${path.module}/../lambda/trigger-api"
-  output_path = "${path.module}/build/trigger-api.zip"
-}
-
 resource "aws_lambda_function" "trigger_api" {
   function_name = "trigger-api-${var.deployment_id}"
-  filename      = data.archive_file.trigger_api_zip.output_path
+  filename      = "${path.module}/build/trigger-api.zip"
   handler       = "handler.lambda_handler"
   runtime       = "python3.10"
   role          = aws_iam_role.lambda_exec.arn
@@ -77,7 +63,7 @@ resource "aws_lambda_function" "trigger_api" {
     variables = {
       UPSTASH_REDIS_REST_URL      = var.redis_url
       UPSTASH_REDIS_REST_TOKEN    = var.redis_password
-      DYNAMO_TABLE   = var.dynamo_table_name
+      DYNAMO_TABLE                = var.dynamo_table_name
     }
   }
 }
