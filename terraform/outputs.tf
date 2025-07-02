3326cf35e34d7e6f66115413ddbdbf9d0ae49330
HugoF-Silva <hugo.fernandes@discente.ufg.br>
2025-07-02 18:47:38 -0300

    Remove unused lambda secret permission output
    Deleted the 'lambda_secret_permission_arn' output from outputs.tf as it is no longer needed.

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,7 +5,3 @@ output "message_checker_url" {
 output "trigger_api_url" {
   value = aws_lambda_function_url.trigger_api_url.function_url
 }
-
-output "lambda_secret_permission_arn" {
-  value = aws_iam_policy.secretsmanager_get.arn
-}
