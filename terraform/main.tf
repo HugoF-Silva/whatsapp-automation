8a79e65069fbbc66179b42707020761ab07cb9e7
HugoF-Silva <hugo.fernandes@discente.ufg.br>
2025-07-03 19:43:01 -0300

    Add INSTANCE_NAME env var to Lambda function
    Introduces the INSTANCE_NAME environment variable to the aws_lambda_function.message_checker resource, allowing the Lambda to access the instance name from Terraform variables.

--- terraform/main.tf
+++ terraform/main.tf
@@ -71,6 +71,7 @@ resource "aws_lambda_function" "message_checker" {
       UPSTASH_REDIS_REST_TOKEN               = var.redis_password
       TRIGGER_API_URL              = aws_lambda_function_url.trigger_api_url.function_url
       GOOGLE_API_KEY = var.google_api_key
+      INSTANCE_NAME = var.instance_name
     }
   }
 }
